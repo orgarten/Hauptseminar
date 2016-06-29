672cb14ce9dc560ac40ffa28c970ec84cf340a70
B4Rkhausen <orellgarten@gmx.de>
2016-06-29 17:56:42 +0200

    added header for excel

--- Flo_octave/writeEXCEL.m
+++ Flo_octave/writeEXCEL.m
@@ -1,4 +1,5 @@
 function [ ans ] = writeEXCEL(filename, content)
 %WRITEEXCEL writes matrix to excel sheet
+  content = [content; {'File' 'ripple' 'sigma' 'dc' 't_diff'}];
   xlswrite(filename,content);
 end;
