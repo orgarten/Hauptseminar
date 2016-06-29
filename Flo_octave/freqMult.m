6f851c62676f0052764a62c8bc29252a9e3b770d
Florian Roth <Florian Roth>
2016-06-29 18:12:29 +0200

    deletes not existign imaginary part

--- Flo_octave/freqMult.m
+++ Flo_octave/freqMult.m
@@ -10,7 +10,7 @@ f_channel_b = fftshift(fft(channel_b));
 correlation = f_channel_a .* conj(f_channel_b);
 correlation_t = ifftshift(ifft(correlation));
 
-correlation = real(correlation_t)/(length(correlation_t)/rate)^2;
+correlation = real(correlation_t/max(correlation_t));
 
 else
 f_channel_a = fft(channel_a);
@@ -21,7 +21,7 @@ correlation_t = ifft(correlation);
 correlation(:,1:length(correlation_t)/2) = correlation_t(:,length(correlation_t)/2+1:length(correlation_t));
 correlation(:,length(correlation_t)/2+1:length(correlation_t)) = correlation_t(:,1:length(correlation_t)/2);
 
-correlation = real(correlation)/max(correlation);
+correlation = real(correlation/max(correlation));
 % norm to length^2 --> (length(correlation)/rate)^2
 end
 
